b4a599f967d2b6c6c4723230eacee3a096c1241a
Craig Campbell <craigsc333@gmail.com>
2010-12-23 00:14:01 -0500

    Added tweet button to all individual pages.

--- reload.py
+++ reload.py
@@ -7,7 +7,6 @@ client = httpclient.HTTPClient()
 try:
 	response = client.fetch("https://graph.facebook.com/search?q=fml&type=post&limit=50")
 	json = escape.json_decode(response.body)
-	print json
 	mc = memcache.Client(['127.0.0.1:11211'], debug=0)
 	mc.set("newest", json)
 except httpclient.HTTPError, e:
